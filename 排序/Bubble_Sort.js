d7819f4d5cb679b82114765554b102443d98d72e
L.Z.Che <FadingLight9291117@126.com>
2019-08-10 22:22:42 +0800

    first commit

---
+++ 排序/Bubble_Sort.js
@@ -0,0 +1,15 @@
+function sort(array) {
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = 0; j < array.length - 1 - i; j++) {
+            if (array[j] > array[j + 1]) {
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
+let array = [2, 5, 2, 32, 6, 12];
+sort(array);
+console.log(array);
