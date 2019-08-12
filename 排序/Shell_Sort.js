76689f757c5aebfa314e6816daae8befeb63e33a
L.Z.Che <FadingLight9291117@126.com>
2019-08-12 13:13:04 +0800

    update 希尔排序

--- 排序/Shell_Sort.js
+++ 排序/Shell_Sort.js
@@ -3,7 +3,7 @@
  */
 function Shell_Sort(a, n) {
     for (let i = parseInt(n / 2); i > 0; i = parseInt(i / 2)) { // 希尔增量序列
-        for (let j = i; j < n; j++) {
+        for (let j = i; j < n; j++) { // 插入排序
             let temp = a[j];
             for (var k = j; k >= i && a[k - i] > temp; k -= i) {
                 a[k] = a[k - i];
@@ -12,6 +12,6 @@ function Shell_Sort(a, n) {
         }
     }
 }
-let a = [2, 5,2,53,12,32,78,5,9,12,94,12,32,443,76,12,32,909, 2, 3];
+let a = [2, 5, 2, 53, 12, 32, 78, 5, 9, 12, 94, 12, 32, 443, 76, 12, 32, 909, 2, 3];
 Shell_Sort(a, a.length);
 console.log(a);
